9fd0bad8fc26185a5d8e32136ecea4d6ac963cdf
anhtoan07112002 <anhtoan07112002@gmail.com>
2025-04-27 00:20:48 +0700

    provide detail value for chaturl

--- src/main/java/com/flowiseai/getscheme/config/ChatbotConfig.java
+++ src/main/java/com/flowiseai/getscheme/config/ChatbotConfig.java
@@ -2,13 +2,18 @@ package com.flowiseai.getscheme.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 public class ChatbotConfig {
+    
+    private final String chatbotUrl;
 
-
-    @Value("${chatbot.url:http://localhost:3000/chatbot/default}")
-    private String chatbotUrl;
+    @Autowired
+    public ChatbotConfig(Environment env) {
+        this.chatbotUrl = env.getProperty("chatbot.url", "http://localhost:3000/chatbot/default");
+    }
 
     public String getChatbotUrl() {
         return chatbotUrl;
