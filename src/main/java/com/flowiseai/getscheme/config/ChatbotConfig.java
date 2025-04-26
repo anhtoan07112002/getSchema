f7b3df44d49f1c82bf6ad44a570aa1d9aafd1009
anhtoan07112002 <anhtoan07112002@gmail.com>
2025-04-27 00:30:00 +0700

    modify to deploy

--- src/main/java/com/flowiseai/getscheme/config/ChatbotConfig.java
+++ src/main/java/com/flowiseai/getscheme/config/ChatbotConfig.java
@@ -4,15 +4,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Configuration
 public class ChatbotConfig {
-    
+    private static final Logger logger = LoggerFactory.getLogger(ChatbotConfig.class);
     private final String chatbotUrl;
 
-    @Autowired
-    public ChatbotConfig(Environment env) {
-        this.chatbotUrl = env.getProperty("chatbot.url", "http://localhost:3000/chatbot/default");
+    public ChatbotConfig() {
+        this.chatbotUrl = "http://localhost:3000/chatbot/default";
+        logger.info("Initializing ChatbotConfig with default URL: {}", this.chatbotUrl);
     }
 
     public String getChatbotUrl() {
