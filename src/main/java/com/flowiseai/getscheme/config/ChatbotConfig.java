efc9c80810868117975751dfb9786ed722aeb2a8
anhtoan07112002 <anhtoan07112002@gmail.com>
2025-04-27 00:08:02 +0700

    modify config file to fix can't set chatbot.url

--- src/main/java/com/flowiseai/getscheme/config/ChatbotConfig.java
+++ src/main/java/com/flowiseai/getscheme/config/ChatbotConfig.java
@@ -5,8 +5,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChatbotConfig {
-    
-    @Value("${chatbot.url}")
+
+
+    @Value("${chatbot.url:http://localhost:3000/chatbot/default}")
     private String chatbotUrl;
 
     public String getChatbotUrl() {
